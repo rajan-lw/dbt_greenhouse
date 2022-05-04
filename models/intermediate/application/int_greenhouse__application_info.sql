15dc59c4b3ce9d65d4b41e751a7e4d94f3f68e06
Rajan Patel <98428783+rajan-lw@users.noreply.github.com>
2022-05-03 22:28:10 -0500

    Adding additional fields from "offer" and "rejection_reason" to Application Enhancement Table TMP

--- models/intermediate/application/int_greenhouse__application_info.sql
+++ models/intermediate/application/int_greenhouse__application_info.sql
@@ -28,6 +28,19 @@ source as (
     from {{ var('source') }}
 ),
 
+rejection_reason as (
+
+    select 
+        *
+    from {{ var('rejection_reason')}}
+),
+
+offer as (
+
+    select *
+    from {{ var('offer')}}
+),
+
 activity as (
 
     select 
@@ -95,14 +108,51 @@ join_info as (
         job_stage.stage_name as current_job_stage,
         source.source_name as sourced_from,
         source.source_type_name as sourced_from_type,
-        activity.count_activities,
 
-        job.job_title,
-        job.status as job_status,
-        job.hiring_managers,
-        job.job_id,
-        job.requisition_id as job_requisition_id,
-        job.sourcers as job_sourcers
+        rejection_reason.rejection_reason_type_name,
+        rejection_reason.reason as rejection_reason,
+
+            offer.CUSTOM_ANNUAL_BONUS as offer_CUSTOM_ANNUAL_BONUS
+        ,   offer.CUSTOM_ANNUAL_VARIABLE_COMP_AMOUNT as offer_CUSTOM_ANNUAL_VARIABLE_COMP_AMOUNT
+        ,   offer.CUSTOM_ANNUAL_COMPENSATION_OFFER_1616528073_398702 as offer_CUSTOM_ANNUAL_COMPENSATION_OFFER_1616528073_398702
+        ,   offer.CUSTOM_CAR_ALLOWANCE as offer_CUSTOM_CAR_ALLOWANCE
+        ,   offer.CUSTOM_DRAW_PERIOD as offer_CUSTOM_DRAW_PERIOD
+        ,   offer.CUSTOM_EMPLOYMENT_TYPE as offer_CUSTOM_EMPLOYMENT_TYPE
+        ,   offer.CUSTOM_END_DATE as offer_CUSTOM_END_DATE
+        ,   offer.CUSTOM_FINAL_OFFER_LETTER_TITLE as offer_CUSTOM_FINAL_OFFER_LETTER_TITLE
+        ,   offer.CUSTOM_HIRE_TYPE as offer_CUSTOM_HIRE_TYPE
+        ,   offer.CUSTOM_HOURLY_RATE_INTERNS_ONLY as offer_CUSTOM_HOURLY_RATE_INTERNS_ONLY
+        ,   offer.CUSTOM_HOUSING_STIPEND as offer_CUSTOM_HOUSING_STIPEND
+        ,   offer.CUSTOM_IMMIGRATION_STATUS as offer_CUSTOM_IMMIGRATION_STATUS
+        ,   offer.CUSTOM_INTERNAL_LOCATION as offer_CUSTOM_INTERNAL_LOCATION
+        ,   offer.CUSTOM_JOB_LEVEL as offer_CUSTOM_JOB_LEVEL
+        ,   offer.CUSTOM_JOB_PROFILE as offer_CUSTOM_JOB_PROFILE
+        ,   offer.CUSTOM_MANAGERS_NAME as offer_CUSTOM_MANAGERS_NAME
+        ,   offer.CUSTOM_MONTHLY_DRAW_AMOUNT as offer_CUSTOM_MONTHLY_DRAW_AMOUNT
+        ,   offer.CUSTOM_PERFORMANCE_BONUS_CS_FIELD_MARKETING_SALES_OPS_ as offer_CUSTOM_PERFORMANCE_BONUS_CS_FIELD_MARKETING_SALES_OPS_
+        ,   offer.CUSTOM_RELOCATION_BONUS_AMOUT as offer_CUSTOM_RELOCATION_BONUS_AMOUT
+        ,   offer.CUSTOM_SIGN_ON_BONUS as offer_CUSTOM_SIGN_ON_BONUS
+        ,   offer.CUSTOM_STOCK_OPTIONS_AMOUNT as offer_CUSTOM_STOCK_OPTIONS_AMOUNT
+        ,   offer.CUSTOM_TARGET_ACCEPTANCE_WINDOW as offer_CUSTOM_TARGET_ACCEPTANCE_WINDOW
+        ,   offer.CUSTOM_VARIABLE_COMP_PAYMENT_FREQUENCY as offer_CUSTOM_VARIABLE_COMP_PAYMENT_FREQUENCY
+        ,   offer.CUSTOM_WORKED_AT_COMPETITOR_IN_LAST_1_YEAR as offer_CUSTOM_WORKED_AT_COMPETITOR_IN_LAST_1_YEAR
+        ,   offer.MAX_OFFER_VERSION as offer_max
+        ,   offer.SENT_AT as offer_sent_at
+        ,   offer.STARTS_AT as offer_starts_at
+        ,   offer.STATUS as offer_status
+        ,   offer.VERSION as offer_version
+        ,   offer.CREATED_AT as offer_created_at
+        ,   offer.UPDATED_AT as offer_updated_at
+        ,   offer.RESOLVED_AT as offer_resolved_at
+
+        ,   activity.count_activities
+
+        ,   job.job_title
+        ,   job.status as job_status
+        ,   job.hiring_managers
+        ,   job.job_id
+        , job.requisition_id as job_requisition_id
+        , job.sourcers as job_sourcers
 
         {% if var('greenhouse_using_job_office', True) %}
         ,
@@ -111,8 +161,8 @@ join_info as (
 
         {% if var('greenhouse_using_job_department', True) %}
         ,
-        job.departments as job_departments,
-        job.parent_departments as job_parent_departments
+        job.departments as job_departments
+        ,job.parent_departments as job_parent_departments
         {% endif %}
 
         {% if var('greenhouse_using_prospects', true) %}
@@ -139,6 +189,10 @@ join_info as (
         on application.current_stage_id = job_stage.job_stage_id
     left join source
         on application.source_id = source.source_id
+    left join rejection_reason
+        on application.rejected_reason_id = rejection_reason.rejection_reason_type_id
+    left join offer
+        on application.application_id = offer.application_id
     left join activity
         on activity.candidate_id = candidate.candidate_id
     left join job_application
